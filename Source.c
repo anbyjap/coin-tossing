d52e1ed82f743b3c2c7bf831ab594a3bd494cd0c
OF5 <haruru898@gmail.com>
2020-08-04 03:41:16 +0900

    HoT: Initial revision

---
+++ Source.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int x,h = 0,t = 0;
+
+	printf("tossing a coin...\n");
+
+	for(int i = 1; i <= 3; i++) {
+		x = rand() % 2;
+		printf("round%d :", i);
+		if (x == 0) {
+			printf("Heads\n");
+			h++;
+		}
+		else {
+			printf("Tails\n");
+			t++;
+		}
+	}
+	printf("Heads: %d, Tails: %d\n", h, t);
+}
